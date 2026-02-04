1f4356101083807b88399442badf60fc1d0cb600
jvukicev <jovan.vukicevic@oracle.com>
2026-02-04 12:42:09 +0100

    Exclude StaticResourceTest

--- test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
+++ test-suite-http-server-tck-netty/src/test/java/io/micronaut/http/server/tck/netty/tests/NettyHttpServerTestSuite.java
@@ -1,5 +1,6 @@
 package io.micronaut.http.server.tck.netty.tests;
 
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -7,5 +8,7 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Netty")
+// fails on native
+@ExcludeClassNamePatterns("io.micronaut.http.server.tck.tests.staticresources.StaticResourceTest")
 public class NettyHttpServerTestSuite {
 }
